7b08b82ea0b7b66f2422a2ddb0dbaf24feb4ef96
Furkan Sarı <furkansarip@gmail.com>
2023-05-29 16:00:32 +0300

    Project initialization

---
+++ MyPlanner/MyPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlanner
+//
+//  Created by Furkan Sarı on 29.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
