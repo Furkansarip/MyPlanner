76c25ef3d8b2bc9a9d0bd3ce75459d70862e88b4
Furkan Sarı <furkansarip@gmail.com>
2023-05-30 21:07:55 +0300

    Tabbar Setup & Created ViewControllers

--- MyPlanner/MyPlanner/Scenes/Login/LoginViewController.swift
+++ MyPlanner/MyPlanner/Scenes/Login/LoginViewController.swift
@@ -40,26 +40,24 @@ class LoginViewController: UIViewController {
         loginButton.tintColor = .purple
         loginButton.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
         
+        let padding: CGFloat = 10
         view.addSubview(titleLabel)
         view.addSubview(loginView)
         view.addSubview(loginButton)
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
-            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
             titleLabel.bottomAnchor.constraint(equalTo: loginView.topAnchor, constant: -25),
             
             loginView.heightAnchor.constraint(equalToConstant: 100),
             loginView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
-            loginView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            loginView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            loginView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+            loginView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
             
             loginButton.topAnchor.constraint(equalTo: loginView.bottomAnchor, constant: 25),
-            loginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            loginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            
-            
-            
+            loginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+            loginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
         ])
     }
     
--- MyPlanner/MyPlanner/Scenes/Main/MainViewController.swift
+++ MyPlanner/MyPlanner/Scenes/Main/MainViewController.swift
@@ -13,9 +13,27 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
         view.backgroundColor = .blue
         title = "Main"
+        setupTabs()
     }
     
     private func setupTabs() {
+        let summaryVC = SummaryViewController()
+        let goalsVC = GoalsViewController()
+        let reminderVC = ReminderViewController()
+        let settingsVC = SettingsViewController()
+        
+        UITabBar.appearance().backgroundColor = .systemBackground
+        let tag1 = UINavigationController(rootViewController: summaryVC)
+        let tag2 = UINavigationController(rootViewController: goalsVC)
+        let tag3 = UINavigationController(rootViewController: reminderVC)
+        let tag4 = UINavigationController(rootViewController: settingsVC)
+        
+        tag1.tabBarItem = UITabBarItem(title: "Summary", image: UIImage(systemName: "chart.pie"), tag: 0)
+        tag2.tabBarItem = UITabBarItem(title: "Goals", image: UIImage(systemName: "arrow.triangle.pull"), tag: 1)
+        tag3.tabBarItem = UITabBarItem(title: "Reminder", image: UIImage(systemName: "exclamationmark.arrow.circlepath"), tag: 2)
+        tag4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 3)
+        
+        setViewControllers([tag1,tag2,tag3,tag4], animated: true)
         
     }
     
