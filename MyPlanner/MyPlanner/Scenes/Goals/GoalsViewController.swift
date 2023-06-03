a750c199965d49b2e8118e04c383e3a4c0c093c3
Furkan Sarı <furkansarip@gmail.com>
2023-06-03 13:32:18 +0300

    Created BaseViewController & Deleted SettingsViewController

--- MyPlanner/MyPlanner/Scenes/Goals/GoalsViewController.swift
+++ MyPlanner/MyPlanner/Scenes/Goals/GoalsViewController.swift
@@ -7,23 +7,12 @@
 
 import UIKit
 
-class GoalsViewController: UIViewController {
+class GoalsViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        title = "Goals"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
